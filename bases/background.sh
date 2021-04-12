31a87525a4cf5c32ec92702efc9bc84e4c8b5280
ETRS-Linux <82364372+ETRS-Linux@users.noreply.github.com>
2021-04-12 22:03:19 +0200

    Update background.sh

--- bases/background.sh
+++ bases/background.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-touch /root/createdFromStep2BackgroundScript >& /dev/null
-touch /root/animaux >& /dev/null
-touch /root/cafes >& /dev/null
-touch /root/motos >& /dev/null
+touch /root/createdFromStep2BackgroundScript &> /dev/null
+touch /root/animaux &> /dev/null
+touch /root/cafes &> /dev/null
+touch /root/motos &> /dev/null
 
-echo -e "Triumph" >> /root/motos >& /dev/null
-echo -e "Yamaha" >> /root/motos >& /dev/null
-echo -e "Suzuki" >> /root/motos >& /dev/null
-echo -e "Pangolin" >> /root/animaux >& /dev/null
-echo -e "Pandas" >> /root/animaux >& /dev/null
-echo -e "Nounours" >> /root/animaux >& /dev/null
-echo -e "Arabica" >> /root/cafes >& /dev/null
+echo -e "Triumph" >> /root/motos &> /dev/null
+echo -e "Yamaha" >> /root/motos &> /dev/null
+echo -e "Suzuki" >> /root/motos &> /dev/null
+echo -e "Pangolin" >> /root/animaux &> /dev/null
+echo -e "Pandas" >> /root/animaux &> /dev/null
+echo -e "Nounours" >> /root/animaux &> /dev/null
+echo -e "Arabica" >> /root/cafes &> /dev/null
 
-mkdir /root/rep1 >& /dev/null
-mkdir /root/rep2 >& /dev/null
+mkdir /root/rep1 &> /dev/null
+mkdir /root/rep2 &> /dev/null
 
-touch /root/rep1/file >& /dev/null
-touch /root/rep2/certificat >& /dev/null
+touch /root/rep1/file &> /dev/null
+touch /root/rep2/certificat &> /dev/null
