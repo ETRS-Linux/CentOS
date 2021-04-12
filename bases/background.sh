5f8a157e532463092b07706416b1a0aed07bed45
ETRS-Linux <82364372+ETRS-Linux@users.noreply.github.com>
2021-04-12 21:49:12 +0200

    Add files via upload

--- bases/background.sh
+++ bases/background.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo "Triumph" >> /root/motos; echo "Pandas" >> /root/animaux; echo "Nounours" >> /root/animaux; echo "Arabica" >> /root/cafes; mkdir /root/rep1; touch /root/rep1/file
+touch /root/createdFromStep2BackgroundScript
+touch /root/animaux
+touch /root/cafes
+touch /root/motos
+
+echo -e "Triumph" >> /root/motos
+echo -e "Yamaha" >> /root/motos
+echo -e "Suzuki" >> /root/motos
+echo -e "Pangolin" >> /root/animaux
+echo -e "Pandas" >> /root/animaux
+echo -e "Nounours" >> /root/animaux
+echo -e "Arabica" >> /root/cafes
+
+mkdir /root/rep1
+mkdir /root/rep2
+
+touch /root/rep1/file
+touch /root/rep2/certificat
