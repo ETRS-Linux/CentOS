dc275359b232c7ebaa89bb847151868e9a67736f
ETRS-Linux <82364372+ETRS-Linux@users.noreply.github.com>
2021-04-12 21:20:24 +0200

    Add files via upload

--- bases/step2-background.sh
+++ bases/step2-background.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-touch /root/createdFromStep2BackgroundScript
-touch /root/animaux
-touch /root/cafes
-touch /root/motos
-echo "Triumph\nYamaha\nSuzuki" > /root/motos
-echo "Pangolin\nPandas\nNounours" > /root/animaux
+touch /root/createdFromStep2BackgroundScript &>/dev/null
+touch /root/animaux &>/dev/null
+touch /root/cafes &>/dev/null
+touch /root/motos &>/dev/null
+
+echo "Triumph" > /root/motos
+echo "Yamaha" > /root/motos
+echo "Suzuki" > /root/motos
+echo "Pangolin" > /root/animaux
+echo "Pandas" > /root/animaux
+echo "Nounours" > /root/animaux
 echo "Arabica" > /root/cafes
 
