9f7f42178b2bfd7a846bb8ddeb3e6ade309a7e71
ETRS-Linux <82364372+ETRS-Linux@users.noreply.github.com>
2021-04-12 21:58:27 +0200

    Update background.sh

--- bases/background.sh
+++ bases/background.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-touch /root/createdFromStep2BackgroundScript > /dev/null 2>&1
-touch /root/animaux > /dev/null 2>&1
-touch /root/cafes > /dev/null 2>&1
-touch /root/motos > /dev/null 2>&1
+touch /root/createdFromStep2BackgroundScript >& /dev/null
+touch /root/animaux >& /dev/null
+touch /root/cafes >& /dev/null
+touch /root/motos >& /dev/null
 
-echo -e "Triumph" >> /root/motos > /dev/null 2>&1
-echo -e "Yamaha" >> /root/motos > /dev/null 2>&1
-echo -e "Suzuki" >> /root/motos > /dev/null 2>&1
-echo -e "Pangolin" >> /root/animaux > /dev/null 2>&1
-echo -e "Pandas" >> /root/animaux > /dev/null 2>&1
-echo -e "Nounours" >> /root/animaux > /dev/null 2>&1
-echo -e "Arabica" >> /root/cafes > /dev/null 2>&1
+echo -e "Triumph" >> /root/motos >& /dev/null
+echo -e "Yamaha" >> /root/motos >& /dev/null
+echo -e "Suzuki" >> /root/motos >& /dev/null
+echo -e "Pangolin" >> /root/animaux >& /dev/null
+echo -e "Pandas" >> /root/animaux >& /dev/null
+echo -e "Nounours" >> /root/animaux >& /dev/null
+echo -e "Arabica" >> /root/cafes >& /dev/null
 
-mkdir /root/rep1 > /dev/null 2>&1
-mkdir /root/rep2 > /dev/null 2>&1
+mkdir /root/rep1 >& /dev/null
+mkdir /root/rep2 >& /dev/null
 
-touch /root/rep1/file > /dev/null 2>&1
-touch /root/rep2/certificat > /dev/null 2>&1
+touch /root/rep1/file >& /dev/null
+touch /root/rep2/certificat >& /dev/null
