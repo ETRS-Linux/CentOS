ed77971cd353b4e989defb320de3515dc56fc4b4
ETRS-Linux <82364372+ETRS-Linux@users.noreply.github.com>
2021-04-15 17:14:54 +0200

    Update background.sh

--- bases/background.sh
+++ bases/background.sh
@@ -4,7 +4,7 @@ clear
 echo "Préparation du TP en cours..."
 touch /root/motos 1>/dev/null 2>&1
 touch /root/animaux 1>/dev/null 2>&1
-touch /root/cafe 1>/dev/null 2>&1
+touch /root/cafes 1>/dev/null 2>&1
 
 mkdir /root/rep1 1>/dev/null 2>&1
 mkdir /root/rep2 1>/dev/null 2>&1
@@ -14,10 +14,12 @@ yum -y install tree --downloadonly --downloaddir=/BoiteAOutils/RPM/ 1>/dev/null 
 
 touch /root/rep1/file 1>/dev/null 2>&1
 touch /root/rep2/certificat 1>/dev/null 2>&1
-cd /root 1>/dev/null 2>&1
+
 clear
 echo "Vous pouvez commencer le TP ! "
 EOF
+
 clear
+cd /root
 chmod u+x /tmp/prepa_tp.sh
 /tmp/prepa_tp.sh
