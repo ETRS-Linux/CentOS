39244c3c15f11646f572a736342274212f20fc54
ETRS-Linux <82364372+ETRS-Linux@users.noreply.github.com>
2021-04-15 16:40:43 +0200

    Update background.sh

--- bases/background.sh
+++ bases/background.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-echo "Triumph" >> /root/motos
-echo "Yamaha" >> /root/motos
-echo "Suzuki" >> /root/motos
-echo "Pangolin" >> /root/animaux
-echo "Pandas" >> /root/animaux
-echo "Nounours" >> /root/animaux
-echo "Arabica" >> /root/cafes
+touch /root/motos
+touch /root/animaux
+touch /root/cafe
 
 mkdir /root/rep1
 mkdir /root/rep2
