02894ff08044af0a6ab62e3fb446640b9d886e82
ETRS-Linux <82364372+ETRS-Linux@users.noreply.github.com>
2021-04-15 10:52:05 +0200

    Update background.sh

--- cours_linux/bases/background.sh
+++ cours_linux/bases/background.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-touch ~/animaux
-touch ~/cafes
-touch ~/motos
+touch /animaux
+touch /cafes
+touch /motos
 
-echo -e "Triumph" >> ~/motos
-echo -e "Yamaha" >> ~/motos
-echo -e "Suzuki" >> ~/motos
-echo -e "Pangolin" >> ~/animaux
-echo -e "Pandas" >> ~/animaux
-echo -e "Nounours" >> ~/animaux
-echo -e "Arabica" >> ~/cafes
+echo -e "Triumph" >> /motos
+echo -e "Yamaha" >> /motos
+echo -e "Suzuki" >> /motos
+echo -e "Pangolin" >> /animaux
+echo -e "Pandas" >> /animaux
+echo -e "Nounours" >> /animaux
+echo -e "Arabica" >> /cafes
 
-mkdir ~/rep1
-mkdir ~/rep2
+mkdir /rep1
+mkdir /rep2
 
-touch ~/rep1/file1
-touch ~/rep2/file2
+touch /rep1/file1
+touch /rep2/file2
