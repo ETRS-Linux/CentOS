e81dbe08c93aa1258069dcee3081200011dee2a5
ETRS-Linux <82364372+ETRS-Linux@users.noreply.github.com>
2021-04-15 16:52:53 +0200

    Update background.sh

--- bases/background.sh
+++ bases/background.sh
@@ -6,6 +6,11 @@ touch /root/cafe
 
 mkdir /root/rep1
 mkdir /root/rep2
+mkdir -p /BoiteAOutils/RPM
+
+yum -y install tree --downloadonly --downloaddir=/BoiteAOutils/RPM/
 
 touch /root/rep1/file
 touch /root/rep2/certificat
+cd /root
+clear
