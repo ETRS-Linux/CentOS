138837349f274fb0de2284526a278d801c5e963f
ETRS-Linux <82364372+ETRS-Linux@users.noreply.github.com>
2021-04-15 16:05:21 +0200

    Update step2-background.sh

--- bases/step2-background.sh
+++ bases/step2-background.sh
@@ -1,7 +1,5 @@
-clear
 cat << EOF > /tmp/script1.sh
 #!/bin/bash
-
 echo "Triumph" >> /root/motos
 echo "Yamaha" >> /root/motos
 echo "Suzuki" >> /root/motos
@@ -10,8 +8,6 @@ echo "Pandas" >> /root/animaux
 echo "Nounours" >> /root/animaux
 echo "Arabica" >> /root/cafes
 EOF
-
 chmod u+x /tmp/script1.sh
 /tmp/script1.sh
-cd /root
 clear
