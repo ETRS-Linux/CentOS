67c0528b9765bc1f78454812771d7e4b0b727a80
ETRS-Linux <82364372+ETRS-Linux@users.noreply.github.com>
2021-04-15 16:54:18 +0200

    Update background.sh

--- bases/background.sh
+++ bases/background.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+echo "Préparation du TP en cours..."
+touch /root/motos 1>/dev/null 2>&1
+touch /root/animaux 1>/dev/null 2>&1
+touch /root/cafe 1>/dev/null 2>&1
 
-touch /root/motos
-touch /root/animaux
-touch /root/cafe
+mkdir /root/rep1 1>/dev/null 2>&1
+mkdir /root/rep2 1>/dev/null 2>&1
+mkdir -p /BoiteAOutils/RPM 1>/dev/null 2>&1
 
-mkdir /root/rep1
-mkdir /root/rep2
-mkdir -p /BoiteAOutils/RPM
+yum -y install tree --downloadonly --downloaddir=/BoiteAOutils/RPM/ 1>/dev/null 2>&1
 
-yum -y install tree --downloadonly --downloaddir=/BoiteAOutils/RPM/
-
-touch /root/rep1/file
-touch /root/rep2/certificat
-cd /root
+touch /root/rep1/file 1>/dev/null 2>&1
+touch /root/rep2/certificat 1>/dev/null 2>&1
+cd /root 1>/dev/null 2>&1
 clear
+echo "Vous pouvez commencer le TP !"
